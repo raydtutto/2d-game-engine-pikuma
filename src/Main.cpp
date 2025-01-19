cf2b18a564fd6191418b8bbee2390fc107ceaccb
raydtutto <raydtutto24@gmail.com>
2025-01-19 17:26:36 +0200

    master | update files

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <SDL.h>
 
 int main() {
+    // Initialize SDL library
+    SDL_Init(SDL_INIT_EVERYTHING);
     std::cout << "Hello, world!" << std::endl;
     return 0;
 }
