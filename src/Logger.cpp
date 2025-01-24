9142f9da7e1ff1b573031850824bcd4520f2d202
raydtutto <raydtutto24@gmail.com>
2025-01-24 22:36:35 +0200

    master | add spdlog

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -5,10 +5,17 @@
 #include <chrono>
 #include <ctime>
 #include <iomanip>
+#include <spdlog/spdlog.h>
 #include <sstream>
 
 // Define the vector
 std::vector<LogEntry> Logger::messages;
+
+// TODO: Make singleton
+// TODO: std::vector<LogEntry> change to pointer
+// TODO: add singleton destructor to clean up messages std::vector<LogEntry*>
+// TODO: Study Alexandresku singleton pattern
+
 
 // Get the current timestamp
 std::string getTimeStamp() {
@@ -28,48 +35,33 @@ std::string getTimeStamp() {
 }
 
 void Logger::Log(const std::string& message) {
+    spdlog::info(message);
+
     LogEntry logEntry;
     logEntry.type = LOG_INFO;
-    logEntry.message = "LOG: [" + getTimeStamp() + "] - " + message;
-
-    // ANSI escape code for green color
-    const std::string greenText = "\033[32m";
-    const std::string resetText = "\033[0m";
-
-    // Output text in color
-    std::cout << greenText << logEntry.message << resetText << std::endl;
+    logEntry.message = "[" + getTimeStamp() + "] [info] " + message;
 
     // Add to messages
     messages.push_back(logEntry);
 }
 
 void Logger::Warning(const std::string& message) {
+    spdlog::warn(message);
+
     LogEntry logEntry;
     logEntry.type = LOG_WARNING;
-    logEntry.message = "WRN: [" + getTimeStamp() + "] - " + message;
-
-    // ANSI escape code for yellow color
-    const std::string yellowText = "\033[33m";
-    const std::string resetText = "\033[0m";
-
-    // Output text in color
-    std::cout << yellowText << logEntry.message << resetText << std::endl;
+    logEntry.message = "[" + getTimeStamp() + "] [warn] " + message;
 
     // Add to messages
     messages.push_back(logEntry);
 }
 
 void Logger::Error(const std::string& message) {
+    spdlog::error(message);
+
     LogEntry logEntry;
     logEntry.type = LOG_ERROR;
-    logEntry.message = "ERR: [" + getTimeStamp() + "] - " + message;
-
-    // ANSI escape code for red color
-    const std::string redText = "\033[31m";
-    const std::string resetText = "\033[0m";
-
-    // Output text in color
-    std::cout << redText << logEntry.message << resetText << std::endl;
+    logEntry.message = "[" + getTimeStamp() + "] [error] " + message;
 
     // Add to messages
     messages.push_back(logEntry);
