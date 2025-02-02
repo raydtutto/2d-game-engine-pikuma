aa430430494518783cc786a5ebbfa354e94904d2
raydtutto <raydtutto24@gmail.com>
2025-02-02 23:45:57 +0200

    master | Entity Component System WIP

--- src/ECS/ECS.cpp
+++ src/ECS/ECS.cpp
@@ -1,5 +1,72 @@
 #include "ECS.h"
 
+#include "../Logger/Logger.h"
+
+// Initializes static methods from header
+int IComponent::nextId = 0;
+
 int Entity::GetId() const {
     return id;
+}
+
+void System::AddEntityToSystem(Entity entity) {
+    entities.push_back(entity);
+}
+
+void System::RemoveEntityToSystem(Entity entity) {
+    // Rely on Entity::operator==
+    entities.erase(std::remove(entities.begin(), entities.end(), entity), entities.end());
+}
+
+// Returns a reference to the entities vector, not a copy of the vector
+[[nodiscard]] const std::vector<Entity>& System::GetSystemEntities() const {
+    return entities;
+}
+
+[[nodiscard]] const Signature& System::GetComponentSignature() const {
+    return componentSignature;
+}
+
+Entity Registry::CreateEntity() {
+    unsigned int entityId = numEntities++;
+
+    // Increases the size of `entityComponentSignatures` if there is more to add
+    if (entityId >= entityComponentSignatures.size()) {
+        entityComponentSignatures.resize(entityId + 1);
+    }
+
+    // Creates new entity
+    Entity entity(entityId);
+
+    // Insert new entity into the line
+    entitiesToBeAdded.insert(entity);
+
+    Logger::Log("Entity created with id = " + std::to_string(entityId));
+    return entity;
+}
+
+void Registry::AddEntityToSystems(Entity entity) {
+    // Get entity id
+    const auto entityId = entity.GetId();
+
+    // Match `entityComponentSignature` <----> `systemComponentSignature`
+    const auto entityComponentSignature = entityComponentSignatures[entityId];
+
+    // Search all the systems within an unordered map
+    for (auto& system: systems) {
+        const auto& systemComponentSignature = system.second->GetComponentSignature();
+
+        // Bitwise comparison between the entity and system signatures
+        bool isInterested = (entityComponentSignature & systemComponentSignature) == systemComponentSignature;
+
+        // Add entity to the system
+        if (isInterested) {
+            system.second->AddEntityToSystem(entity);
+        }
+    }
+}
+
+
+void Registry::Update() {
+
 }
--- src/ECS/ECS.h
+++ src/ECS/ECS.h
@@ -2,6 +2,9 @@
 #define ECS_H
 
 #include <bitset>
+#include <set>
+#include <typeindex>
+#include <unordered_map>
 #include <vector>
 
 constexpr unsigned int MAX_COMPONENTS = 32;
@@ -11,15 +14,55 @@ constexpr unsigned int MAX_COMPONENTS = 32;
 //------------------------------------------------------------------------
 typedef std::bitset<MAX_COMPONENTS> Signature;
 
-class Component {
-    //
+// Class template. A parent class for components
+struct IComponent {
+protected:
+    static int nextId;
 };
 
+//------------------------------------------------------------------------
+// Components
+//------------------------------------------------------------------------
+// Class template that is used to assign a unique id to a component type
+template <typename TComponent>
+class Component : public IComponent {
+    // Returns the unique id of Component<T>
+    static int GetId() { // will exist for each component type separately
+        static auto id = nextId++;
+        return id;
+    }
+};
+
+//------------------------------------------------------------------------
+// Entity
+//------------------------------------------------------------------------
 class Entity {
     int id;
+
 public:
-    Entity(int id): id(id) {}; // Constructor, initialize automatically
+    Entity(unsigned int id)
+        : id(id){};  // Constructor, initialize automatically
+    Entity(const Entity& entity) = default;
     int GetId() const;
+
+    // Operator overloading
+    Entity& operator=(const Entity& other) = default;
+
+    bool operator==(const Entity& other) const {
+        return id == other.id;
+    }
+
+    bool operator!=(const Entity& other) const {
+        return id != other.id;
+    }
+
+    bool operator<(const Entity& other) const {
+        return id < other.id;
+    }
+
+    bool operator>(const Entity& other) const {
+        return id > other.id;
+    }
 };
 
 //------------------------------------------------------------------------
@@ -28,18 +71,236 @@ public:
 class System {
     Signature componentSignature;
     std::vector<Entity> entities;
+
 public:
     System() = default;
-    ~System() = default;
+    virtual ~System() = default;
 
     void AddEntityToSystem(Entity entity);
     void RemoveEntityToSystem(Entity entity);
-    std::vector<Entity> GetSystemEntities() const;
-    Signature& GetComponentSignature() const;
+    [[nodiscard]] const std::vector<Entity>& GetSystemEntities() const;
+    [[nodiscard]] const Signature& GetComponentSignature() const;
+
+    // Defines the component type that entities must have to be considered by the system
+    template <typename TComponent>
+    void RequireComponent();
 };
 
-class Registry {
-    //
+//------------------------------------------------------------------------
+// Pool of components
+//------------------------------------------------------------------------
+class IPool {
+public:
+    virtual ~IPool(){};
 };
+
+// Collection of objects of type T
+template <typename T>
+class Pool : public IPool {
+    std::vector<T> data;
+
+public:
+    Pool(int size = 100) {
+        data.resize(size);
+    }
+
+    virtual ~Pool() = default;  // compiler will generate the default implementation of the destructor
+
+    bool isEmpty() const {
+        return data.empty();
+    }
+
+    int GetSize() const {
+        return data.size();
+    }
+
+    void Resize(int n) {
+        data.resize(n);
+    }
+
+    void Clear() {
+        data.clear();
+    }
+
+    void Add(T object) {
+        data.push_back(object);
+    }
+
+    void Set(int index, T object) {
+        data[index] = object;
+    }
+
+    T& Get(int index) {
+        return static_cast<T&>(data[index]);
+    }
+
+    // Operator overloading
+    T& operator[](unsigned int index) {
+        return data[index];
+    }
+};
+
+//------------------------------------------------------------------------
+// Registry
+//------------------------------------------------------------------------
+// Manages the creation/destruction of entities and adding/removing components and systems
+class Registry {
+    // Keep track of how many entities were added to the scene
+    unsigned int numEntities = 0;
+
+    // Set of entities awaiting creation/destruction in the next `Update()`
+    std::set<Entity> entitiesToBeAdded;
+    std::set<Entity> entitiesToBeKilled;
+
+    // Vector of component pools.
+    // Each pool contains all the data for a certain component type
+    // [vector index = componentId], [pool index = entityId]
+    std::vector<IPool*> componentPools;
+
+    // Vector of component signatures.
+    // The signature lets us know which components are turned "on" for an entity
+    // [ vector index = entity id ]
+    std::vector<Signature> entityComponentSignatures;
+
+    // Map of active systems [ index = system typeid ]
+    std::unordered_map<std::type_index, System*> systems;
+
+public:
+    Registry() = default;
+
+    void Update();
+
+    // Entity management
+    Entity CreateEntity();
+
+    // Checks the component signature of an entity
+    // and add the entity to the systems that are interested in it
+    void AddEntityToSystems(Entity entity);
+
+    // void KillEntity(Entity entity);
+
+    // Component management
+    template <typename TComponent, typename... TArgs>
+    void AddComponent(Entity entity, TArgs&&... args);
+
+    template <typename T>
+    void RemoveComponent(Entity entity);
+
+    template <typename T>
+    bool HasComponent(Entity entity) const;
+
+    // template <typename T>
+    // void GetComponent(Entity entity);
+
+    // System management
+    template <typename TSystem, typename... TArgs>
+    void AddSystem(TArgs&&... args);
+
+    template <typename TSystem>
+    void RemoveSystem();
+
+    template <typename TSystem>
+    bool HasSystem() const;
+
+    template <typename TSystem>
+    TSystem GetSystem() const;
+};
+
+//------------------------------------------------------------------------
+// Function templates
+//------------------------------------------------------------------------
+template <typename TComponent>
+void System::RequireComponent() {
+    const auto componentId = Component<TComponent>::GetId();
+    componentSignature.set(componentId);
+};
+
+template <typename TSystem, typename... TArgs>
+void Registry::AddSystem(TArgs&&... args) {
+    // Create new system
+    // TODO: replace a raw pointer with a smart pointer
+    TSystem* newSystem(new TSystem(std::forward<TArgs>(args)...));
+
+    // Add the system to an unordered map
+    systems.insert(std::make_pair(std::type_index(typeid(TSystem)), newSystem)); // pair(key, value)
+}
+
+template <typename TSystem>
+void Registry::RemoveSystem() {
+    // Get system id
+    const auto system = systems.find(std::type_index(typeid(TSystem)));
+
+    // Remove the system from the unordered map
+    systems.erase(system);
+}
+
+template <typename TSystem>
+bool Registry::HasSystem() const {
+    return systems.find(std::type_index(typeid(TSystem))) != systems.end();
+}
+
+template <typename TSystem>
+TSystem Registry::GetSystem() const {
+    // Get system id
+    const auto system = systems.find(std::type_index(typeid(TSystem)));
+
+    // Returns the system with a correct type
+    // '*->first' gets the key, '*->second' gets the value
+    return std::static_pointer_cast<TSystem>(system->second);
+}
+
+template <typename TComponent, typename... TArgs>
+void Registry::AddComponent(Entity entity, TArgs&&... args) {
+    // Get a component/entity id
+    const auto componentId = Component<TComponent>::GetId();
+    const auto entityId = entity.GetId();
+
+    // Resize the pools of components
+    if (componentId >= componentPools.size())
+        componentPools.resize(componentId + 1, nullptr);
+
+    // If nothing within the current position
+    if (!componentPools[componentId]) {
+        // TODO: replace a raw pointer with a smart pointer
+        Pool<TComponent>* newComponentPool = new Pool<TComponent>();
+        componentPools[componentId] = newComponentPool;  // assigns the current position
+    }
+
+    // Get the component pool
+    Pool<TComponent>* componentPool = componentPools[componentId];
+
+    // Resize current component pool
+    if (entityId >= componentPool->GetSize())
+        componentPool->Resize(numEntities);
+
+    // Creating a new component
+    TComponent newComponent(std::forward<TArgs>(args)...);
+
+    // Set the position
+    componentPool->Set(entityId, newComponent);
+
+    // Enable the bitset signature
+    entityComponentSignatures[entityId].set(componentId);
+}
+
+template <typename T>
+void Registry::RemoveComponent(Entity entity) {
+    // Get a component/entity id
+    const auto componentId = Component<T>::GetId();
+    const auto entityId = entity.GetId();
+
+    // Disable the bitset signature
+    entityComponentSignatures[entityId].set(componentId, false);
+}
+
+template <typename T>
+bool Registry::HasComponent (Entity entity) const {
+    // Get a component/entity id
+    const auto componentId = Component<T>::GetId();
+    const auto entityId = entity.GetId();
+
+    // Return the component state
+    return entityComponentSignatures[entityId].test(componentId);
+}
 
 #endif  // ECS_H
